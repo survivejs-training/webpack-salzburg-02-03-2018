5d1f1d7cfc3d33a533f2c35ba3ad75a6077edb58
Juho Vepsalainen <bebraw@gmail.com>
2018-03-02 15:18:05 +0100

    chore: Compose configuration

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,29 +1,32 @@
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  devServer: {
-    // Display only errors to reduce the amount of output.
-    stats: "errors-only",
+const parts = require("./webpack.parts");
 
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: options.host || "0.0.0.0";
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-
-    // overlay: true is equivalent
-    overlay: {
-      errors: true,
-      warnings: true,
-    },
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
---
+++ demo/webpack.parts.js
@@ -0,0 +1,11 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: "errors-only",
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
